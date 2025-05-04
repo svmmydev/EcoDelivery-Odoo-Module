08c34f1d56fab2a7ea4012bd921fe2d16c247c1d
Sammy <svmmydev@gmail.com>
2025-05-04 13:56:49 +0200

    Etapa 1: Create model EnvioEco + Quick Fick: TransporteEco refactorized

---
+++ models/envioeco.py
@@ -0,0 +1,32 @@
+
+from odoo import fields, models, api
+from odoo.exceptions import ValidationError
+
+
+class EnvioEco(models.Model):
+    _name = 'envioeco.envioeco'
+    _description = 'envioeco'
+    _rec_name = 'pedido'
+    pedido = fields.Many2one(comodel_name='sale.order', required=True)
+    direccion_entrega = fields.Char('Dirección de entrega', required=True)
+    fecha_solicitud = fields.Date(string='Fecha de solicitud', required=True, default=fields.Date.today)
+    tipo_transporte = fields.Many2one(comodel_name='transporteeco.transporteeco', string='Transporte del envío')
+    fecha_entrega_prevista = fields.Date(string='Fecha de entrega prevista', required=True, default=fields.Date.today)
+    estado = fields.Selection([
+        ('pendiente_envio', 'Pendiente de envío'),
+        ('en_reparto', 'En reparto'),
+        ('entregado', 'Entregado'),
+        ('no_entregado', 'No entregado')
+    ], string='Estado del envío',
+        default='pendiente_envio',
+        required=True,
+        help='Selecciona el estado del envío.')
+    fecha_entrega = fields.Date(string='Fecha de entrega')
+
+    @api.constrains('fecha_solicitud', 'fecha_entrega_prevista')
+    def _check_fechas(self):
+        for record in self:
+            if record.fecha_solicitud and record.fecha_entrega_prevista:
+                if record.fecha_solicitud > record.fecha_entrega_prevista:
+                    raise ValidationError('La fecha de solicitud no puede ser posterior a la fecha de entrega prevista.')
+    
--- models/transporteeco.py
+++ models/transporteeco.py
@@ -1,5 +1,6 @@
 
-from odoo import fields, models, api, exceptions
+from odoo import fields, models
+
 
 class TransporteEco(models.Model):
     _name = 'transporteeco.transporteeco'
@@ -8,16 +9,15 @@ class TransporteEco(models.Model):
     transporte = fields.Selection([
         ('bicicleta', 'Bicicleta'),
         ('patinete', 'Patinete'),
-        ('furgoneta', 'Furgoneta'),
-        ('camion', 'Camion')
+        ('furgoneta_elec', 'Furgoneta Eléctrica')
     ], string='Tipo de transporte',
         default='bicicleta',
         required=True,
         help='Elige el tipo de transporte deseado.')
     descripcion = fields.Char('Descripción', required=True)
-    envios = fields.One2many(comodel_name='envioeco.envioeco', string='Envíos en el transporte')
-
+    envios = fields.One2many(comodel_name='envioeco.envioeco', inverse_name='tipo_transporte', string='Envíos en el transporte')
 
     _sql_constraints = [
         ('name_uniq', 'unique (transporte)', '¡Transporte ya existente!' ),
     ]
+    
